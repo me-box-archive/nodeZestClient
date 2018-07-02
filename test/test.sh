4288819370f6ca72784be50cbc168b99e73ec900
ToshBrown <Toshbrown@gmail.com>
2018-07-02 15:10:10 +0100

    Update to zest 0.0.8
    Post can now return data

--- test/test.sh
+++ test/test.sh
@@ -6,47 +6,47 @@ source test/utils.sh
 
 CMD="node ./client/client.js "
 
-EXPECTED="created"
-RES=$($CMD --method post --format json --path /kv/test --payload "{\"name\":\"tosh\",\"age\":38}")
+EXPECTED=""
+RES=$($CMD --method post --format json --path /kv/test/data --payload "{\"name\":\"tosh\",\"age\":38}")
 test_assert "$EXPECTED" "$RES" "Test KV POST JSON "
 
 EXPECTED='{"name":"tosh","age":38}'
-RES=$($CMD --method get --format json --path /kv/test)
+RES=$($CMD --method get --format json --path /kv/test/data)
 test_assert "$EXPECTED" "$RES" "Test KV GET JSON "
 
-EXPECTED="created"
-RES=$($CMD --method post --format text --path /kv/test --payload "{\"name\":\"Tosh\",\"age\":37}")
+EXPECTED=""
+RES=$($CMD --method post --format text --path /kv/test/data --payload "{\"name\":\"Tosh\",\"age\":37}")
 test_assert "$EXPECTED" "$RES" "Test KV POST TEXT "
 
 EXPECTED='{"name":"Tosh","age":37}'
-RES=$($CMD --method get --format text --path /kv/test)
+RES=$($CMD --method get --format text --path /kv/test/data)
 test_assert "$EXPECTED" "$RES" "Test KV GET TEXT "
 
-EXPECTED="created"
-RES=$($CMD --method post --format binary --path /kv/test --payload "{\"name\":\"tosh\",\"age\":36}")
+EXPECTED=""
+RES=$($CMD --method post --format binary --path /kv/test/data --payload "{\"name\":\"tosh\",\"age\":36}")
 test_assert "$EXPECTED" "$RES" "Test KV POST BINARY "
 
 EXPECTED='{"name":"tosh","age":36}'
-RES=$($CMD --method get --format binary --path /kv/test)
+RES=$($CMD --method get --format binary --path /kv/test/data)
 test_assert "$EXPECTED" "$RES" "Test KV GET BINARY "
 
 
-EXPECTED="created"
-RES=$($CMD --method post --format json --path /ts/test --payload "{\"name\":\"tosh\",\"age\":38}")
+EXPECTED=""
+RES=$($CMD --method post --format json --path /ts/blob/test --payload "{\"name\":\"tosh\",\"age\":38}")
 test_assert "$EXPECTED" "$RES" "Test TS POST JSON "
 
-EXPECTED="created"
-RES=$($CMD --method post --format json --path /ts/test --payload "{\"name\":\"tosh\",\"age\":39}")
+EXPECTED=""
+RES=$($CMD --method post --format json --path /ts/blob/test --payload "{\"name\":\"tosh\",\"age\":39}")
 test_assert "$EXPECTED" "$RES" "Test TS POST JSON "
 
-EXPECTED="created"
-RES=$($CMD --method post --format json --path /ts/test --payload "{\"name\":\"tosh\",\"age\":40}")
+EXPECTED=""
+RES=$($CMD --method post --format json --path /ts/blob/test --payload "{\"name\":\"tosh\",\"age\":40}")
 test_assert "$EXPECTED" "$RES" "Test TS POST JSON "
 
 EXPECTED='{"name":"tosh","age":40}'
-RES=$($CMD --method get --format json --path /ts/test/latest)
+RES=$($CMD --method get --format json --path /ts/blob/test/latest)
 test_contains "$EXPECTED" "$RES" "Test TS GET LATEST JSON "
 
 EXPECTED='{"name":"tosh","age":39}'
-RES=$($CMD --method get --format json --path /ts/test/last/2)
+RES=$($CMD --method get --format json --path /ts/blob/test/last/2)
 test_contains "$EXPECTED" "$RES" "Test TS GET LAST 2 JSON "
